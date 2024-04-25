5fd9a24154b7b7d21f9ac4370fec13a2070a7055
Yastrebova E.A <yastrebova.yelena@internet.ru>
2024-04-25 15:28:16 +0700

    Изменили поведение программы при наличии пользовательских параметров в командной строке

--- args.c
+++ args.c
@@ -1,14 +1,18 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
 int main(int argc, char *argv[], char *envp[]) {
 	printf("Количество параметров, переданных программе: %d" , argc);
-	for(int i=0; i<argc; i++) {
-		printf("Параметр %d: %s\n", i, argv[i]);
+	if (argc>1) {
+		for(int i=0; i<argc; i++) {
+			printf("Параметр %d: %s\n", i, argv[i]);
+		}
 	}
-	int j=0;
-	while (envp[j]!=NULL) {
+	else {
+		int j=0;
+		while (envp[j]!=NULL) {
 		printf("Параметр окружения %d: %s\n", j, envp[j]);
 		j++;
 	}
